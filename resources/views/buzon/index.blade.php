b486e8bf44ededda74a7ecbcc0521f28da58269f
handblack <soporte@miasoftware.net>
2025-01-09 00:16:13 -0500

    f

--- resources/views/buzon/index.blade.php
+++ resources/views/buzon/index.blade.php
@@ -4,9 +4,9 @@
 @section('breadcrumb')
 <div class="content-header pb-0">
 	<div class="container-fluid">
-		<div class="row ">
+		<div class="row mb-4">
 			<div class="col-sm-6">
-				<h1 class="m-0"><i class="fas fa-users fa-fw"></i> Buzon</h1>
+				<h1 class="m-0"><i class="fas fa-stopwatch fa-fw"></i> Buzon</h1>
 			</div>
 			<div class="col-sm-6">
 				<ol class="breadcrumb float-sm-right">
@@ -15,7 +15,7 @@
 				</ol>
 			</div>
 		</div>
-        <div class="row mb-2">
+        <div class="row mb-0">
             <div class="col-lg-3 col-6">
                 <!-- small card -->
                 <div class="small-box bg-warning">
@@ -41,11 +41,8 @@
 
 @section('content')
 <div class="card">
-    <div class="card-header">
-
-    </div>
-    <div class="card-body">
-        <table>
+    <div class="card-body table-responsive p-0">
+        <table class="table table-sm">
             <thead>
                 <tr>
                     <th>Fecha</th>
