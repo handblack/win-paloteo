70ba5e1df10acdfc0593318a253657b9b8e71a00
handblack <soporte@miasoftware.net>
2025-01-09 00:03:28 -0500

    add index

--- app/Http/Controllers/BuzonController.php
+++ app/Http/Controllers/BuzonController.php
@@ -11,7 +11,7 @@ class BuzonController extends Controller
      */
     public function index()
     {
-        //
+        return view('buzon.index');
     }
 
     /**
--- resources/views/buzon/index.blade.php
+++ resources/views/buzon/index.blade.php
@@ -2,4 +2,5 @@
 
 @section('content')
     Aqui mostramos
+    
 @endsection
